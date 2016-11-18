0dda4e815cc7060e1c154ab9d18724403f07c212
Armando Castillo <armando.castillo@masnegocio.com>
2016-11-18 15:23:58 -0600

    Grunt war

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,12 @@
+/**
+ * npm install
+ *
+ * npm install -g grunt-cli
+ *
+ * npm install grunt-war --save-dev
+ *
+ * grunt
+ */
 
 module.exports = function (grunt) {
 
@@ -17,12 +26,28 @@ module.exports = function (grunt) {
                     livereload: true
                 }
             }
+        },
+        war: {
+            target: {
+                options: {
+                    war_dist_folder: 'build',    /* Folder where to generate the WAR. */
+                    war_name: 'dn9soluciones'                    /* The name fo the WAR file (.war will be the extension) */
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'web',
+                        src: ['**'],
+                        dest: ''
+                    }
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-war');
 
     grunt.registerTask('default', ['connect', 'watch']);
-
 };
